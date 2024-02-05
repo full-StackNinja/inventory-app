0959e9efd220a3610bf40b1ab892ca081dfd6c90
Imran Hussain <imranhussain399@gmail.com>
2024-02-05 20:24:13 +0500

    Remove inventory-db module

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require("morgan");
 require("dotenv").config();
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const inventoryDB = require("./inventory-db");
+const catalogRouter = require("./routes/catalog");
 
 const mongoose = require("mongoose");
 
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
