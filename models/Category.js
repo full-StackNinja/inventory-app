299f91bff58473988a5dc74c2cb3a0a24545d22e
Imran Hussain <imranhussain399@gmail.com>
2024-02-05 20:26:08 +0500

    change maxLength for description field

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength:100 },
-  description: { type: String, required: true, maxLength:100 },
+  description: { type: String, required: true, maxLength:1500 },
 });
 
 CategorySchema.virtual("url").get(function () {
