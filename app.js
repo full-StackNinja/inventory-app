5dac10fff943b63423618dd8c911aefe4555d443
Imran Hussain <imranhussain399@gmail.com>
2024-02-05 09:56:42 +0500

    Create project skeleton using express-generator

---
+++ app.js
@@ -0,0 +1,55 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+require("dotenv").config();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const inventoryDB = require("./inventory-db");
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const app = express();
+
+const main = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("connected to database!");
+};
+
+main().catch((err) => {
+  console.log("database error", err);
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
