4090654a506afff5d445614935356b147216263c
Imran Hussain <imranhussain399@gmail.com>
2024-02-05 22:38:52 +0500

    Define controller functions for index and category detail routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,11 +4,23 @@ const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.render("index", { title: "Categories" });
+  const categories_list = await Category.find({}, "name")
+    .sort({ name: -1 })
+    .exec();
+  res.render("index", {
+    title: "Categories",
+    categories_list,
+    description:
+      "Shop millions of articles which falls under following categories:",
+  });
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented category detail page");
+  const [category, category_items] = await Promise.all([
+    Category.findById(req.params.id),
+    Item.find({ category: req.params.id }, "name"),
+  ]);
+  res.render("category_detail", { category, category_items });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
