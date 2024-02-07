bf562df249881f6866bdad5bb963159130240ebe
Imran Hussain <imranhussain399@gmail.com>
2024-02-07 23:24:18 +0500

    Implement route handlers for item update route

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -21,9 +21,7 @@ exports.item_create_post = [
     .isInt()
     .withMessage("Stock must be an integer")
     .escape(),
-  body("price")
-    .trim()
-    .escape(),
+  body("price").trim().escape(),
   body("description").trim().escape(),
   asyncHandler(async (req, res, next) => {
     const item = new Item({
@@ -39,12 +37,37 @@ exports.item_create_post = [
 ];
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented item update get");
+  const [item, category_list] = await Promise.all([
+    Item.findById(req.params.id).exec(),
+    Category.find({}).sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("item_form", { title: "Update Item", item, category_list });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented item update post");
-});
+exports.item_update_post = [
+  body("name").trim().escape(),
+  body("category").not().isEmpty().withMessage("Category is required"),
+  body("stock", "stock must be an integer value")
+    .trim()
+    .isInt()
+    .withMessage("Stock must be an integer")
+    .escape(),
+  body("price").trim().escape(),
+  body("description").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const item = new Item({
+      name: req.body.name,
+      category: req.body.category,
+      stock: req.body.stock,
+      price: req.body.price,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, item);
+    res.redirect(updatedItem.url);
+  }),
+];
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
   res.send("Not Implemented item delete get");
