bb04ecb3d38808be4f2efea106be3976fd332fd6
Imran Hussain <imranhussain399@gmail.com>
2024-02-07 23:15:02 +0500

    Implement route handlers for creating new item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -9,12 +9,34 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented item create get");
+  const category_list = await Category.find({}).sort({ name: 1 }).exec();
+  res.render("item_form", { title: "Add New Item", category_list });
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented item create post");
-});
+exports.item_create_post = [
+  body("name").trim().escape(),
+  body("category").not().isEmpty().withMessage("Category is required"),
+  body("stock", "stock must be an integer value")
+    .trim()
+    .isInt()
+    .withMessage("Stock must be an integer")
+    .escape(),
+  body("price")
+    .trim()
+    .escape(),
+  body("description").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const item = new Item({
+      name: req.body.name,
+      category: req.body.category,
+      stock: req.body.stock,
+      price: req.body.price,
+      description: req.body.description,
+    });
+    await item.save();
+    res.redirect(item.url);
+  }),
+];
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
   res.send("Not Implemented item update get");
