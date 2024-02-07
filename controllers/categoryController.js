6ef9a1f56f9391b1a996addb0d6398ba167ed38b
Imran Hussain <imranhussain399@gmail.com>
2024-02-07 17:25:50 +0500

    Add route handler functions for different routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,13 +3,17 @@ const Item = require("../models/Item");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-exports.index = asyncHandler(async (req, res, next) => {
+exports.categories_list = asyncHandler(async (req, res, next) => {
   const categories_list = await Category.find({}, "name")
     .sort({ name: -1 })
     .exec();
+  res.locals.categories_list = categories_list;
+  next();
+});
+
+exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", {
     title: "Categories",
-    categories_list,
     description:
       "Shop millions of articles which falls under following categories:",
   });
@@ -24,12 +28,50 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented create category get");
+  res.render("category_form", { title: "Add New Category" });
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented category create post");
-});
+exports.category_create_post = [
+  body("name")
+    .trim()
+    .not()
+    .isAlpha()
+    .withMessage("name should be alphabetical only")
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("Description should be atleast 10 chars long")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create New Category",
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const categoryExist = await Category.findOne({ name: category.name })
+        .collation({
+          locale: "en",
+          strength: 2,
+        })
+        .exec();
+      if (categoryExist) {
+        res.redirect(categoryExist.url);
+      } else await category.save();
+      res.redirect(category.url);
+    }
+  }),
+];
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
   res.send("Not implemented category update get");
